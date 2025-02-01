a769d6fb3a656e9d8da4512e72387de16d36a10a
sovahem <sovannara.hem.dev@gmail.com>
2025-02-01 12:52:09 +0100

    define column

--- src/main/java/com/ecommerce/model/Product.java
+++ src/main/java/com/ecommerce/model/Product.java
@@ -2,12 +2,19 @@ package com.ecommerce.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Set;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -39,18 +46,55 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Set<Category> categories;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String description;
-    private double price;
+    
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal price;
+
+    @Column(precision = 3, scale = 2)
     private BigDecimal rating;
+
+    @Column(name = "number_of_reviews")
     private Integer numberOfReviews;
+
+    @Column(nullable = false)
     private Boolean active;
+
+    @Column(columnDefinition = "jsonb")
     private String attributes;
+
+    @Column(columnDefinition = "jsonb")
     private String interactions;
+
+    @Version
     private Long version;
+
+    @Column(name = "stock_quantity")
     private Integer stockInQuantity;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+     @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+    
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 
 } 
 
