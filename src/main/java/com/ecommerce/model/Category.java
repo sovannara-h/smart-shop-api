8f249feacb30cca8ebd2efbbe322b1d6972d1283
sovahem <sovannara.hem.dev@gmail.com>
2025-02-01 13:20:23 +0100

    add validation

--- src/main/java/com/ecommerce/model/Category.java
+++ src/main/java/com/ecommerce/model/Category.java
@@ -10,6 +10,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -33,11 +36,14 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Le nom de la catégorie ne peut pas être vide")
+    @Size(min = 2, max = 50, message = "Le nom doit contenir entre 2 et 50 caractères")
     @Column(nullable = false, unique = true)
     private String name;
 
+    @NotNull(message = "Le statut actif/inactif doit être défini")
     @Column(nullable = false)
-    private Boolean active =  true;
+    private Boolean active = true;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
