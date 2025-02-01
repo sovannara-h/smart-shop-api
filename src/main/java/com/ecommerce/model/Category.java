59e91b9155b0ddd41ff49f672c8daf69ec0ded69
sovahem <sovannara.hem.dev@gmail.com>
2025-02-01 12:27:38 +0100

    add category model

---
+++ src/main/java/com/ecommerce/model/Category.java
@@ -0,0 +1,37 @@
+package com.ecommerce.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+// Fields:
+// - id 
+// - name 
+// - active 
+// - createdAt
+// - updatedAt
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Boolean active;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
