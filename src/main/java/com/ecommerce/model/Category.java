4068b97d4b19b65f3353b662b6a57043f0a3d39b
sovahem <sovannara.hem.dev@gmail.com>
2025-02-01 13:12:55 +0100

    add validation

--- src/main/java/com/ecommerce/model/Category.java
+++ src/main/java/com/ecommerce/model/Category.java
@@ -1,12 +1,15 @@
 package com.ecommerce.model;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -44,6 +47,9 @@ public class Category {
 
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @ManyToMany(mappedBy = "categories", fetch = FetchType.LAZY)
+    private Set<Product> products;
 
     @PrePersist
     protected void onCreate() {
