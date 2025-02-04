f30e6c751d03f9d233465968fc0431652cda99b0
sovahem <sovannara.hem.dev@gmail.com>
2025-02-04 11:48:19 +0100

    rate limite config

---
+++ src/main/java/com/ecommerce/config/RateLimitConfig.java
@@ -0,0 +1,20 @@
+package com.ecommerce.config;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+
+@Configuration
+public class RateLimitConfig {
+    @Bean
+    public RateLimiterConfig productRateLimiterConfig() {
+        return RateLimiterConfig.custom()
+            .limitForPeriod(100)
+            .limitRefreshPeriod(Duration.ofMinutes(1))
+            .timeoutDuration(Duration.ofSeconds(1))
+            .build();
+    }
+} 
